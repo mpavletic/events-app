88ec713d3ff5242aeb27f53d67b1d0ca0bca2e68
Martin Pavletic <mpavletic.dev@gmail.com>
2021-06-18 12:56:54 -0300

    Display a list of events in the home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../api/event.service';
+import { IEvent } from '../api/interfaces/ievent';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  events: IEvent[] = [];
 
-  constructor() {}
+  constructor(
+    private eventService: EventService
+  ) {}
+
+  public ngOnInit() {
+    this.eventService.getAll().subscribe( events => this.events = events);
+  }
 
 }
